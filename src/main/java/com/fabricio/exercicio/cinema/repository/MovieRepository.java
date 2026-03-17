f5ebc31ea9bf311dd41d71b3738ce28a2a7bfd1b
Fabpont <fabriciopontes02.fa@gmail.com>
2026-03-17 01:05:43 -0300

    feat(MovieRepository): Create a method to save data in a txt file

--- src/main/java/com/fabricio/exercicio/cinema/repository/MovieRepository.java
+++ src/main/java/com/fabricio/exercicio/cinema/repository/MovieRepository.java
@@ -1,4 +1,24 @@
 package com.fabricio.exercicio.cinema.repository;
 
+import com.fabricio.exercicio.cinema.model.Movie;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MovieRepository {
+    private final String ARQUIVO_MOVIE = "movie.txt";
+
+    public void salvarFilme(Movie movie) throws IOException {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(ARQUIVO_MOVIE, true))) {
+            bw.write(
+                            "Titulo do filme: " + movie.getTitulo()  + " | " +
+                            "Diretor: "+ movie.getDirector() + " | " +
+                            "Avaliação: " + movie.getRating()  + " | " +
+                            "Data de Lançamento: " + movie.getReleaseDate()  + " | " +
+                            "Genero: "  + movie.getGenre()
+            );
+            bw.newLine();
+        }
+    }
 }
